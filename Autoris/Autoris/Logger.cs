25121aa4a6a4d9a99acd99d8f4f40399d2a89442
VolkovaEkaterina <asu-dev007@yandex.ru>
2024-07-02 15:26:52 +0500

    Создала класс логирования об ошибке

---
+++ Autoris/Autoris/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Autoris
+{
+    /// <summary>
+    /// Класс для логирования событий
+    /// </summary>
+    public class Logger
+    {
+        /// <summary>
+        /// Логирует событие
+        /// </summary>
+        /// <param name="eventMessage"></param>
+        public void WriteEvent(string eventMessage)
+        {
+            Console.WriteLine($"[{DateTime.UtcNow}] Event: {eventMessage}");
+        }
+
+        /// <summary>
+        /// Логирует ошибку
+        /// </summary>
+        /// <param name="errorMessage"></param>
+        public void WriteError(string errorMessage)
+        {
+            Console.WriteLine($"[{DateTime.UtcNow}] Error: {errorMessage}");
+        }
+
+
+
+    }
+}
