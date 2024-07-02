614a655181c4bcbf9e9fd2d40a7df4bf3ece1878
VolkovaEkaterina <asu-dev007@yandex.ru>
2024-07-02 16:11:15 +0500

    Добавила UserController и убрала WeatherForecastController

---
+++ Autoris/Autoris/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Autoris.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Autoris.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        /// <summary>
+        /// Конструктор класса UserController
+        /// </summary>
+        public UserController()
+        {
+            var logger = new Logger();
+
+            logger.WriteEvent("Сообщение о событии в программе");
+            logger.WriteError("Сообщение об ошибки в программе");
+
+        }
+
+        [HttpGet]
+        public User GetUser()
+        {
+            return new User
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Иван",
+                LastName = "Иванов",
+                Email = "Ivanov@yandex.ru",
+                Login = "IIvanov",
+                Password = "123456789"
+            };
+        }
+    }
+}
---
+++ Autoris/Autoris/Models/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Autoris.Models
+{
+    /// <summary>
+    /// Класс описывающий сущность пользователя
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// ID в БД
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Логин пользователя
+        /// </summary>
+        public string Login { get; set; }
+
+        /// <summary>
+        /// Пароль пользователя
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Фамилия пользователя
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// E-mail адрес пользователя
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
