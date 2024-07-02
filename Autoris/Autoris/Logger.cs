b8271dbd38beb057f6ac6501b3b999fe992f58c9
VolkovaEkaterina <asu-dev007@yandex.ru>
2024-07-02 16:40:58 +0500

    Добавила логирование событий и ошибок в файл

--- Autoris/Autoris/Logger.cs
+++ Autoris/Autoris/Logger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,20 @@ namespace Autoris
     /// </summary>
     public class Logger : ILogger
     {
+        private string saveDir;
+
+        /// <summary>
+        /// Конструктор класса логирования
+        /// </summary>
+        public Logger()
+        {
+            //Создает новую директорию при каждом запуске
+            saveDir = Path.Combine(Directory.GetCurrentDirectory(), "Logs", DateTime.UtcNow.ToString("dd MM yyyy HH.mm.ss"));
+            if (!Directory.Exists(saveDir))
+                Directory.CreateDirectory(saveDir);
+
+        }
+
         /// <summary>
         /// Логирует ошибку в консоль
         /// </summary>
@@ -36,22 +51,54 @@ namespace Autoris
         }
 
         /// <summary>
+        /// Логирует событие в файл
+        /// </summary>
+        /// <param name="eventMessage"></param>
+        /// <returns></returns>
+        private async Task WriteEventFileAsync(string eventMessage)
+        {
+            // Для логирования данных о запросе используем свойста объекта HttpContext
+            string logMessage = $"[{DateTime.UtcNow}] Event: {eventMessage}\r\n";
+            string logFilePath = Path.Combine(saveDir, "Event.txt");
+
+            await File.AppendAllTextAsync(logFilePath, logMessage);
+        }
+
+        /// <summary>
+        /// Логирует ошибку в файл
+        /// </summary>
+        /// <param name="errorMessage"></param>
+        /// <returns></returns>
+        private async Task WriteErrorFileAsync(string errorMessage)
+        {
+            // Для логирования данных о запросе используем свойста объекта HttpContext
+            string logMessage = $"[{DateTime.UtcNow}] Error: {errorMessage}\r\n";
+            string logFilePath = Path.Combine(saveDir, "Error.txt");
+
+            await File.AppendAllTextAsync(logFilePath, logMessage);
+        }
+
+
+
+        /// <summary>
         /// Логирует событие
         /// </summary>
         /// <param name="eventMessage"></param>
-        public void WriteEvent(string eventMessage)
+        public async void WriteEvent(string eventMessage)
         {
-
+            
             WriteEventConsole(eventMessage);
+            await WriteEventFileAsync(eventMessage);
         }
 
         /// <summary>
         /// Логирует ошибку
         /// </summary>
         /// <param name="errorMessage"></param>
-        public void WriteError(string errorMessage)
+        public async void WriteError(string errorMessage)
         {
             WriteErrorConsole(errorMessage);
+            await WriteErrorFileAsync(errorMessage);
         }
 
 
