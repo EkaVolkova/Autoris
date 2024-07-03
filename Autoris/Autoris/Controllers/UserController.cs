e2c96a8fa1920d8cca04cf8c3b63574a68277e43
VolkovaEkaterina <asu-dev007@yandex.ru>
2024-07-03 08:35:44 +0500

    Добавила модель представления для класса User

--- Autoris/Autoris/Controllers/UserController.cs
+++ Autoris/Autoris/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Autoris.Models;
+using Autoris.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 
@@ -34,6 +35,25 @@ namespace Autoris.Controllers
                 Login = "IIvanov",
                 Password = "123456789"
             };
+        }
+
+        [HttpGet]
+        [Route("viewmodel")]
+        public UserViewModel GetUserViewModel()
+        {
+            User user = new User()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Иван",
+                LastName = "Иванов",
+                Email = "ivan@gmail.com",
+                Password = "11111122222qq",
+                Login = "ivanov"
+            };
+
+            UserViewModel userViewModel = new UserViewModel(user);
+
+            return userViewModel;
         }
     }
 }
