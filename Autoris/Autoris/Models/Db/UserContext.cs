fc41f7f41aee50028712b087d913943bd994ad17
VolkovaEkaterina <asu-dev007@yandex.ru>
2024-07-03 10:12:04 +0500

    Добавила взаимодействие с БД через IUserRepository

---
+++ Autoris/Autoris/Models/Db/UserContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Autoris.Models.Db
+{
+    public class UserContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+
+        // Логика взаимодействия с таблицами в БД
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("Users");
+        }
+    }
+}
