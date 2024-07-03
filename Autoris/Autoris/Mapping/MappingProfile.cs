1489a6118575104d5e73b03d46cf1fbcee9eca96
VolkovaEkaterina <asu-dev007@yandex.ru>
2024-07-03 08:50:45 +0500

    Добавила автомаппинг модели при помощи пакета AutoMapper

---
+++ Autoris/Autoris/Mapping/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Autoris.Models;
+using Autoris.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Autoris.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //Создаем способ получения UserViewModel на основе User
+            CreateMap<User, UserViewModel>()
+                //Для передачи объекта User в качестве параметра для конструктора UserViewModel
+                .ConstructUsing(v => new UserViewModel(v));
+        }
+    }
+}
