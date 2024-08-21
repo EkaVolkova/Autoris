b17f6ee87a6d8625ad1a1ae9123f9559d977e7f7
VolkovaEkaterina <asu-dev007@yandex.ru>
2024-08-21 09:36:57 +0500

    Добавила фильтр исключений

---
+++ Autoris/Autoris/Exceptions/ExceptionHandler.cs
@@ -0,0 +1,32 @@
+﻿using Autoris.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Autoris.Exceptions
+{
+    /// <summary>
+    /// Класс фильтра исключений
+    /// </summary>
+    public class ExceptionHandler : ActionFilterAttribute, IExceptionFilter
+    {
+        /// <summary>
+        /// Логер
+        /// </summary>
+        ILogger _logger = new Logger();
+
+        /// <summary>
+        /// Реакция на событие возникновение исключений
+        /// </summary>
+        /// <param name="context">Текущий контекст</param>
+        public void OnException(ExceptionContext context)
+        {
+            string message = $"Произошла ошибка, администратор сайта уже спешит ее исправить\r\n{context.Exception.Message}";
+            context.Result = new BadRequestObjectResult(message);
+            _logger.WriteError(new Error(message));
+        }
+    }
+}
